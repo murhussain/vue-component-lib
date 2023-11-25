095931cc909a53ba0e81ab368265007b74f7ee9c
Murhussain <husseinmurashi@yahoo.com>
2023-11-25 16:23:45 +0200

    Adding the view plugin file for initializing our components on either app level or component import level

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,13 @@
-import './assets/main.css'
+import './assets/base.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
-import router from './router'
+import ViewerPlugin from "./ViewerPlugin";
 
-const app = createApp(App)
-
+// const app = createApp(App)
+const app = createApp(App);
+// app.use(ViewerPlugin, { img: "https://picsum.photos/1000/1000" });
+app.use(ViewerPlugin);
 app.use(createPinia())
-app.use(router)
-
 app.mount('#app')
